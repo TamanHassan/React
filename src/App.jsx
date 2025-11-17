5fd6532f0301787a269b9c905c4e35599ec02d1e
Taman Hassan <tamanhassan1@gmail.com>
2025-11-17 15:21:51 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import ProjectCard from "./components/ProjectCard";
+
+function App() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Portfolio</h1>
+      <ProjectCard
+        name="React"
+        screenshot="../img"
+        github=
+        tech="React, API Fetch,CSS"
+        description=
+        whatYouLearned=
+        role=
+        challenges=
+      />
+    
+     <ProjectCard
+        name="Reacr"
+        screenshot="../img"
+        github=
+        tech="React, API Fetch,CSS"
+        description=
+        whatYouLearned=
+        role=
+        challenges=
+      />
+
+       <ProjectCard
+        name="Reacr"
+        screenshot="../img"
+        github=
+        tech="React, API Fetch,CSS"
+        description=
+        whatYouLearned=
+        role=
+        challenges=
+      />
+      </div>
+  );
+}
+
+export default App;
+      
---
+++ src/src/components/ProjectCard.jsx
@@ -0,0 +1,51 @@
+function ProjectCard( {
+    name,
+    screenshot,
+    github,
+    tech,
+    description,
+    whatYouLearned,
+    role,
+    challenges,
+} ) {
+    return (
+        <div style={styles.card}>
+            <img src="" alt="" />
+
+            <h2>React</h2>
+            <p><strong> Teach Used:</strong>{tech} </p>
+            <p><strong> Description:</strong>{description} </p>
+            <p><strong> What You Learned:</strong>{whatYouLearned} </p>
+
+            {role && <p><strong> Your Role:</strong>{role} </p>}
+            <p><strong>Challenges Solved:</strong>{challenges} </p>
+
+            <a href={github} target="_blank">View on GitHub</a>
+        </div>
+    );
+    }
+
+    const styles = {
+        card: {
+            border: "1px solid #ddd",
+            padding: "20px",
+            marginBottom: "20px",
+            borderRadius: "10px",
+            width: "350px"
+        },
+        image: {
+            width: "100%",
+            borderRadius: "8px",
+
+        }
+        };
+
+    export default ProjectCard;
+
+
+
+            
+
+
+
+    
